0c5e8c44aed5aeb39828353a5f9e1327f90c0939
Nikitapil <nikitapiliavets@gmail.com>
2023-04-20 15:57:29 +0300

    try to fix deploy with cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from './pipes/validation.pipe';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
